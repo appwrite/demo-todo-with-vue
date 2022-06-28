b83a1576bcdc16891676828c5d31128a5f78f35a
Brandon <robertsbt@gmail.com>
2022-06-28 13:05:37 -0500

    feat: update Appwrite to support 0.15.x

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,6 @@
 export const Server = {
     endpoint : import.meta.env.VITE_APP_ENDPOINT,
     project: import.meta.env.VITE_APP_PROJECT,
-    collectionID : import.meta.env.VITE_APP_COLLECTION_ID
+    collectionID : import.meta.env.VITE_APP_COLLECTION_ID,
+    database: import.meta.env.VITE_APP_DATABASE_ID.toString(),
 }
