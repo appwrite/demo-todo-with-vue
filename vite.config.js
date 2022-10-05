52f5a2b4f1df83a35378439faaee091d461fca58
b5710546232 <safe.suk@gmail.com>
2022-10-05 07:09:26 +0700

    chore: when build remove log

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
+export default ({ mode }) => {
+  return  defineConfig({
+    esbuild: {
+      drop: mode=== 'development' ? [] : ['console', 'debugger']
+    },
+    plugins: [vue()]
 })
+}
